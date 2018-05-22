5f4424a2334d9f45b4a94aaca571f77523ac7e81
Ruslan Kladko <ruslan.kladko@pilulka.cz>
2018-05-22 09:49:02 +0300

    Introduce dynamic models for json

---
+++ src/Model/JsonModel.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Pilulka\CoreApiClient\Model;
+
+
+class JsonModel
+{
+    /** @var array */
+    protected $attributes;
+
+    public function __construct(array $attributes = [])
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function __get(string $attribute)
+    {
+        if (array_key_exists($attribute, $this->attributes)) {
+            return $this->attributes[$attribute];
+        }
+        return null;
+    }
+
+    public function __set(string $attribute, string $value): void
+    {
+        if (array_key_exists($attribute, $this->attributes)) {
+            $this->attributes[$attribute] = $value;
+        }
+    }
+
+    public function __isset(string $attribute): bool
+    {
+        return array_key_exists($attribute, $this->attributes);
+    }
+}
