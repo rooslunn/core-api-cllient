ad1b94661977866a5368564689f59df486236b2b
Ruslan Kladko <ruslan.kladko@pilulka.cz>
2018-05-24 17:10:48 +0300

    Manufacturer/Create done

---
+++ src/Model/Manufacturer/ManufacturerContent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pilulka\CoreApiClient\Model\Manufacturer;
+
+use Pilulka\CoreApiClient\Model\JsonModel;
+
+class ManufacturerContent extends JsonModel
+{
+    /** @var  string|null */
+    private $long;
+
+}
